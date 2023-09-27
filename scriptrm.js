ddc550f3f44036e8895019ae8de666f3270363f5
Sarvesh Suryakant Arondekar <115332581+sarvesh11030801@users.noreply.github.com>
2023-09-27 15:58:53 +0530

    first commit

---
+++ scriptrm.js
@@ -0,0 +1,10 @@
+window.addEventListener("mousemove", function (details) {
+    var rect = document.querySelector("#rect");
+
+    var xval = gsap.utils.mapRange(0, window.innerWidth, 200+ rect.getBoundingClientRect().width / 2, window.innerWidth - (200+ rect.getBoundingClientRect().width / 2), details.clientx)
+    
+    gsap.to("#rect", {
+        left: xval,
+        ease: Power3,
+    });
+});
